827eef902050710528297604d6b2a8aafd468951
Couverabana <couverabana@gmail.com>
2024-05-18 18:32:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// Exemplo de redirecionamento para a página inicial ao clicar no botão "Voltar"
+document.querySelector('button').addEventListener('click', function () {
+  window.location.href = 'index.html';
+});
